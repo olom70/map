b093c150a92d9e314ed443a4cb59ef57b5f2e64b
Claude Moreau <claude.moreau.olom@gmail.com>
2022-05-17 23:21:30 +0200

    pandas exploration

--- interractivepandas.py
+++ interractivepandas.py
@@ -1,6 +1,8 @@
 #%%
 import configparser
 import os
+
+from openpyxl import Workbook
 from lib.tools import check_ini_files_and_return_config_object, create_main_variables_from_config, initialize_db, get_current_session, get_queries
 INIFILE = 'map_indicators.ini'
 config = configparser.ConfigParser()
@@ -22,6 +24,9 @@ import pandas as pd
 sql_query = pd.read_sql(all_queries_in_a_dict['connected_at_least_once'], conn)
 df = pd.DataFrame(sql_query)
 print(df)
+print(f'df.index : {df.index}')
+print(f'df.columns : {df.columns}')
+
 # %%
 df.to_excel(backup_path+"map.xlsx", index=False, sheet_name='connected_at_teast_once')
 
@@ -29,3 +34,28 @@ df.to_excel(backup_path+"map.xlsx", index=False, sheet_name='connected_at_teast_
 with pd.ExcelWriter(backup_path+'map.xlsx',
                     mode='a', if_sheet_exists='replace', engine='openpyxl') as writer:
     df.to_excel(writer, index=False, sheet_name='connected_at_teast_once')
+#%%
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.worksheet.table import Table, TableStyleInfo
+wb = Workbook()
+ws = wb.active
+index = 0
+for index, r in enumerate(dataframe_to_rows(df, index=True, header=True), start=1):
+    ws.append(r)
+print(index)
+ref = 'A1:F'+str(index)
+tab = Table(displayName="connected_at_least_once", ref=ref)
+style = TableStyleInfo(name="TableStyleDark12", showFirstColumn=False,
+                       showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+tab.tableStyleInfo = style
+ws.add_table(tab)
+wb.save(backup_path+'map2.xlsx')
+#%%
+df.plot.barh()
+#%%
+df.assign(entreprise_team=df["entreprise"] + " " + df["team"]+ " " + df["connected_at_least_once"]).groupby(['entreprise_team']).sum().plot.barh()
+
+#%%
+print(df)
+df.groupby(['team']).sum()
