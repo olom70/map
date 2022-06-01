391720a6be82c05916ac5c5b5e5eb123019efd5b
Claude Moreau <claude.moreau.olom@gmail.com>
2022-06-01 22:04:04 +0200

    change import statement and move some files

--- map_indicators.py
+++ map_indicators.py
@@ -1,13 +1,11 @@
 import configparser
 from sys import exit
 import logging
-from prompt_toolkit.validation import Validator
-from prompt_toolkit.shortcuts import yes_no_dialog
-from prompt_toolkit import print_formatted_text, HTML, prompt
+import prompt_toolkit
 # https://htmlcolorcodes.com/fr/noms-de-couleur/
 # https://python-prompt-toolkit.readthedocs.io/en/master/pages/getting_started.html
-from lib.tools import check_ini_files_and_return_config_object, create_main_variables_from_config, initialize_db
-from helpers import are_all_files_ok, validator, validator_yn, welcome, process
+from map import tools
+from map import helpers
 
 logger = logging.getLogger('map_indicator_app')
 logger.setLevel(logging.DEBUG)
@@ -23,7 +21,7 @@ def main():
     INIFILE = 'map_indicators.ini'
     logger.info(f'The name of the ini file is {INIFILE}')
     config = configparser.ConfigParser()
-    config = check_ini_files_and_return_config_object(INIFILE)[0]
+    config = tools.check_ini_files_and_return_config_object(INIFILE)[0]
     if 'Sessions' not in config:
         logger.critical(f'The ini file {INIFILE} is malformed or does not exists. Exiting the application. Check the logs')
         exit()
@@ -31,36 +29,36 @@ def main():
     maindir, separator, file_ext, iniFilesDir, prefix, context, backup_name, log_level = str(), str(), str(), str(), str(), str(), str(), str()
     tables, retailers, toolkit_tables = list(), list(), list()
     retailers_tables = dict()
-    variables_from_ini_in_list = create_main_variables_from_config([config])
+    variables_from_ini_in_list = tools.create_main_variables_from_config([config])
     if variables_from_ini_in_list is None:
         logger.critical('One of the entry in the ini file triggered a critical error. Exiting the application. Check the logs')
         exit()
     maindir, separator, retailers, tables, retailers_tables, toolkit_tables, file_ext, iniFilesDir, prefix, context, backup_name, log_level = variables_from_ini_in_list
     logger.setLevel(log_level)
 
-    are_all_files_ok()
-    input = prompt('Your choice : (y)es/ (c)ancel) : ', validator=validator_yn)
+    helpers.are_all_files_ok()
+    input = prompt_toolkit.prompt('Your choice : (y)es/ (c)ancel) : ', validator=helpers.validator_yn)
     if input == 'c':
-        print_formatted_text(HTML(f'<aaa bg="LightYellow"><HotPink><b>Goodbye !</b></HotPink></aaa>'))
+        prompt_toolkit.print_formatted_text(prompt_toolkit.HTML(f'<aaa bg="LightYellow"><HotPink><b>Goodbye !</b></HotPink></aaa>'))
         logger.info('User canceled the initialisation')
         fh.close()
         exit()
 
-    conn = initialize_db(':memory:', retailers, retailers_tables, toolkit_tables, file_ext, iniFilesDir)[0]
+    conn = tools.initialize_db(':memory:', retailers, retailers_tables, toolkit_tables, file_ext, iniFilesDir)[0]
     if conn is None:
         logger.critical('Initialisation of the databse failed. Exiting the application. Check the logs')
 
 
     while True:
-        welcome()
+        helpers.welcome()
         try:
-            digit_input = int(prompt('Give a number: ', validator=validator))
+            digit_input = int(prompt_toolkit.prompt('Give a number: ', validator=helpers.validator))
         except KeyboardInterrupt:
             continue
         except EOFError:
             break
         else:
-            process(digit_input)
+            helpers.process(digit_input)
     print('GoodBye!')
     fh.close()
 
